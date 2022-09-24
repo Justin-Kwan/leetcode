eff435c9fc3537bd99ce284a1d17f1f8a75acc20
Justin-Kwan <justinpkwan@outlook.com>
2022-09-24 00:54:01 -0400

    did more leetcode

--- kth_largest_element_in_an_array.py
+++ kth_largest_element_in_an_array.py
@@ -1,3 +1,5 @@
+import random
+
 class Solution:
 #     # max heap modifying
 #     def findKthLargest(self, nums: List[int], k: int) -> int:
@@ -14,21 +16,63 @@ class Solution:
 
 #         return curMax
 
-    # min heap non-modifying
+#     # min heap non-modifying
+#     def findKthLargest(self, nums: List[int], k: int) -> int:
+#         if len(nums) == 0:
+#             return 0
+
+#         # use min heap to easily eject numbers below k that we don't care about,
+#         # to optimize average case space complexity when k < len(nums)
+#         kLargestNums = []
+
+#         for num in nums:
+#             heapq.heappush(kLargestNums, num)
+
+#             # if length becomes k+1, remove root which is definitely below kth largest
+#             if len(kLargestNums) > k:
+#                 heapq.heappop(kLargestNums)
+
+#         # kthlargest number sits at root of heap, pushed up by larger numbers
+#         return kLargestNums[0]
+
+    # optimal quick select
     def findKthLargest(self, nums: List[int], k: int) -> int:
         if len(nums) == 0:
             return 0
 
-        # use min heap to easily eject numbers below k that we don't care about,
-        # to optimize average case space complexity when k < len(nums)
-        kLargestNums = []
+        return self.quickSelect(nums, k, 0, len(nums) - 1)
 
-        for num in nums:
-            heapq.heappush(kLargestNums, num)
+    def quickSelect(self, nums: List[int], k: int, start: int, end: int) -> int:
+        # finally reached kth largest number
+        if start == end:
+            return nums[start]
 
-            # if length becomes k+1, remove root which is definitely below kth largest
-            if len(kLargestNums) > k:
-                heapq.heappop(kLargestNums)
+        # pick random pivot to partition numbers around
+        pivotPos = random.randint(start, end)
+        sortedPivotPos = self.partition(nums, pivotPos, start, end)
 
-        # kthlargest number sits at root of heap, pushed up by larger numbers
-        return kLargestNums[0]
+        # pivot number is kth largest, found early
+        if sortedPivotPos == len(nums) - k:
+            return nums[sortedPivotPos]
+        # kth largest in left smaller group
+        elif sortedPivotPos > len(nums) - k:
+            return self.quickSelect(nums, k, start, sortedPivotPos - 1)
+        # kth largest in right larger group
+        else:
+            return self.quickSelect(nums, k, sortedPivotPos + 1, end)
+
+    # given a position of a number to pivot around, return its position in sorted order.
+    def partition(self, nums: List[int], pivotPos: int, start: int, end: int) -> int:
+        # temporarily move pivot number out of the way
+        nums[pivotPos], nums[end] = nums[end], nums[pivotPos]
+
+        nextSmallerPos = start
+        for i in range(start, end):
+            # throw ith number (smaller than pivot) to end of smaller group
+            if nums[i] <= nums[end]:
+                nums[nextSmallerPos], nums[i] = nums[i], nums[nextSmallerPos]
+                nextSmallerPos += 1
+
+        # put pivot number back right after smaller group
+        nums[end], nums[nextSmallerPos] = nums[nextSmallerPos], nums[end]
+        return nextSmallerPos
