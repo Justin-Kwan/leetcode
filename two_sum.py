aaa0739e16d8723aab022c6274c7a6ef394aff91
Justin-Kwan <justinpkwan@outlook.com>
2022-10-08 19:56:17 -0400

    redid a leetcode

--- two_sum.py
+++ two_sum.py
@@ -1,15 +1,17 @@
 class Solution:
-    # returns two indices whose elements sum to target
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        numsLookup = {}
-        
-        # dump array into hashmap
+        # dump all numbers by position into hashmap
+        numsByPos = {}
         for i in range(len(nums)):
-            numsLookup[nums[i]] = i
+            # intentionally overwrite previous duplicate number's position to indicate both 
+            # are not the same element
+            numsByPos[nums[i]] = i
 
+        # find a possible complement of each number to sum to target
         for i in range(len(nums)):
             complement = target - nums[i]
-            
-            if complement in numsLookup and numsLookup[complement] != i:
-                return [ numsLookup[complement], i ]
+
+            # complement of number exists and check both are not the same element
+            # (complement should be ahead of current number for unique or duplicate pairs)
+            if complement in numsByPos and i < numsByPos[complement]:
+                return [i, numsByPos[complement]]
