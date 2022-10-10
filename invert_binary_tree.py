1855379b4711494c410f0352cffc9cf43d413229
Justin-Kwan <justinpkwan@outlook.com>
2022-10-10 02:03:28 -0400

    redid a leetcode

--- invert_binary_tree.py
+++ invert_binary_tree.py
@@ -5,32 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # post-order traversal and return to parent to swap
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root is None:
-            return root
-
-        leftChild = self.invertTree(root.left)
-        rightChild = self.invertTree(root.right)
-
-        root.left = rightChild
-        root.right = leftChild
-
-        return root
-
-#     # pre-order traversal swapping
+#     # preorder traversal swapping
 #     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-#         self.swapLevelNodes(root)
+#         self.invertSubtree(root)
 #         return root
 
-#     # pre-order traversal to swap
-#     def swapLevelNodes(self, root: Optional[TreeNode]):
-#         if root is None:
+#     def invertSubtree(self, root: Optional[TreeNode]) -> None:
+#         if not root:
 #             return
-    
-#         tempLeftChild = root.left
-#         root.left = root.right
-#         root.right = tempLeftChild
 
-#         self.invertTree(root.left)
-#         self.invertTree(root.right)
+#         # swap left and right subtrees, and continue swapping in both
+#         root.left, root.right = root.right, root.left
+
+#         self.invertSubtree(root.left)
+#         self.invertSubtree(root.right)
+
+    # postorder traversal and return parent to swap
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        invertedRight = self.invertTree(root.right)
+        invertedLeft = self.invertTree(root.left)
+
+        root.left = invertedRight
+        root.right = invertedLeft
+
+        return root
