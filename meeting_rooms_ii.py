d01ac6248a051fa575958e2d05fa9f1751ad45c9
Justin-Kwan <justinpkwan@outlook.com>
2022-10-13 18:37:51 -0400

    did another leetcode

--- meeting_rooms_ii.py
+++ meeting_rooms_ii.py
@@ -1,42 +1,45 @@
 class Solution:
-    # optomized track room expiries in min heap
+    # optimal track room expiries in min heap
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # underlying list to store expiries as nodes in heap
         roomExpiries = []
-        intervals.sort(key = lambda interval: interval[0])
-        
-        for interval in intervals:
-            # exists a room expiry and earliest expiring room (root) is before meeting start
-            if roomExpiries and roomExpiries[0] <= interval[0]:
-                heapq.heapreplace(roomExpiries, interval[1])    # pop earliest expiry and add new room expiry as end of meeting
+
+        # sort all meeting time intervals by start time
+        intervals = sorted(intervals, key=lambda interval: interval[0])
+
+        for startTime, endTime in intervals:
+            # can reuse earliest expiring room, pop it and add updated room expiry
+            if roomExpiries and startTime >= roomExpiries[0]:
+                heapq.heapreplace(roomExpiries, endTime)
+            # no rooms exist or new meeting overlaps with others in use (earliest),
+            # add new room 
             else:
-                heapq.heappush(roomExpiries, interval[1])       # checkout new room and add expiry
+                heapq.heappush(roomExpiries, endTime)
+
         return len(roomExpiries)
 
-#     # unoptimized track room expiries in list
+#     # suboptimal list approach
 #     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
 #         roomExpiries = []
 
-#         intervals.sort(key = lambda interval: interval[0])
+#         # sort all meeting time intervals by start time
+#         intervals = sorted(intervals, key=lambda interval: interval[0])
 
-#         for interval in intervals:
-#             earliestRoomExpiryPos = self.findEarliesRoomExpiryPos(roomExpiries)
+#         for startTime, endTime in intervals:
+#             meetingRoomPos = self.findAvailableRoom(startTime, roomExpiries)
 
-#             # if room exists and expires before meeting, meeting can use
-#             if earliestRoomExpiryPos != -1 and roomExpiries[earliestRoomExpiryPos] <= interval[0]:
-#                 roomExpiries[earliestRoomExpiryPos] = interval[1]   # update expiry to end of meeting
+#             # new meeting overlaps with all others in use, requiring new room
+#             if meetingRoomPos < 0:
+#                 roomExpiries.append(endTime)
+#             # update available meeting room with new expiry time
 #             else:
-#                 roomExpiries.append(interval[1])   # checkout meeting room, and set expiry
+#                 roomExpiries[meetingRoomPos] = endTime
 
 #         return len(roomExpiries)
 
-#     def findEarliesRoomExpiryPos(self, roomExpiries: List[int]) -> int:
-#         minRoomExpiry = float('inf')
-#         minRoomExpiryPos = -1
+#     def findAvailableRoom(self, startTime: int, roomExpiries: List[int]) -> int:
+#         # find first available room that expires before new meeting
+#         for i in range(len(roomExpiries)):
+#             if roomExpiries[i] <= startTime:
+#                 return i
 
-#         for i in range(0, len(roomExpiries)):
-#             if roomExpiries[i] < minRoomExpiry:
-#                 minRoomExpiry = roomExpiries[i]
-#                 minRoomExpiryPos = i
-
-#         return minRoomExpiryPos
+#         return -1
