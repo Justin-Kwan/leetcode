68598dac5dd00f66a66505db43ecb00997df002e
Justin Kwan <justinpkwan@outlook.com>
2023-06-30 19:24:09 -0400

    did another leetcode approach

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -1,29 +1,59 @@
 class Solution:
-    # one map bottom up dp approach
+    # optimal greedy approach
     def trap(self, heights: List[int]) -> int:
-        # no water can be trapped with 2 or less blocks
-        if len(heights) <= 2:
-            return 0
-
-        rightCache = [0] * len(heights)
+        # initial max heights up to both ends do not exist
+        maxLeftHeight, maxRightHeight = 0, 0
+        leftPos, rightPos = 0, len(heights) - 1
         trappedWaterCount = 0
 
-        # only build single map of max block heights to right of each block
-        for i in range(len(heights) - 2, -1, -1):
-            maxRightHeight = max(heights[i + 1], rightCache[i + 1])
-            rightCache[i] = maxRightHeight
-
-        # carry over max block height to left of each block without storing,
-        # then compute water trapped at current block
-        maxLeftHeight = 0
-        for i in range(1, len(heights)):
-            maxLeftHeight = max(heights[i - 1], maxLeftHeight)
-            # since water height at current block calculated from ground level,
-            # subtract height of current block to account for overflowing
-            curWaterCount = min(maxLeftHeight, rightCache[i]) - heights[i]
-            trappedWaterCount += max(0, curWaterCount)
+        # keep computing water heights on each side until requiring more
+        # information (taller heights) from other side to optimize current
+        # decision
+        while leftPos <= rightPos:
+            # decide water height at each left position until the max left 
+            # height is taller than max right so far
+            # (stall and try finding  taller block on right side)
+            if maxLeftHeight <= maxRightHeight:
+                curWaterCount = maxLeftHeight - heights[leftPos]
+                trappedWaterCount += max(0, curWaterCount)
+                maxLeftHeight = max(heights[leftPos], maxLeftHeight)
+                leftPos += 1
+            # decide water height at each right position until the max right 
+            # height is taller than max left so far
+            # (stall and try finding taller block on left side)
+            else:
+                curWaterCount = maxRightHeight - heights[rightPos]
+                trappedWaterCount += max(0, curWaterCount)
+                maxRightHeight = max(heights[rightPos], maxRightHeight)
+                rightPos -= 1
 
         return trappedWaterCount
+
+    # # one map bottom up dp approach
+    # def trap(self, heights: List[int]) -> int:
+    #     # no water can be trapped with 2 or less blocks
+    #     if len(heights) <= 2:
+    #         return 0
+
+    #     rightCache = [0] * len(heights)
+    #     trappedWaterCount = 0
+
+    #     # only build single map of max block heights to right of each block
+    #     for i in range(len(heights) - 2, -1, -1):
+    #         maxRightHeight = max(heights[i + 1], rightCache[i + 1])
+    #         rightCache[i] = maxRightHeight
+
+    #     # carry over max block height to left of each block without storing,
+    #     # then compute water trapped at current block
+    #     maxLeftHeight = 0
+    #     for i in range(1, len(heights)):
+    #         maxLeftHeight = max(heights[i - 1], maxLeftHeight)
+    #         # since water height at current block calculated from ground level,
+    #         # subtract height of current block to account for overflowing
+    #         curWaterCount = min(maxLeftHeight, rightCache[i]) - heights[i]
+    #         trappedWaterCount += max(0, curWaterCount)
+
+    #     return trappedWaterCount
 
     # # two map bottom up dp approach
     # def trap(self, heights: List[int]) -> int:
