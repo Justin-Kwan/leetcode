b445fda12d802233051af43a1e41c948907944c1
Justin Kwan <justinpkwan@outlook.com>
2023-08-22 16:30:52 -0400

    did another leetcode

---
+++ minimum_number_of_arrows_to_burst_balloons.py
@@ -0,0 +1,23 @@
+class Solution:
+    # optimal sorting approach
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if not points:
+            return 0
+
+        # sort all ballon intervals by start positions to compare overlaps
+        sortedPoints = sorted(points, key=lambda point: point[0])
+        arrowCount = 0
+        overlapEnd = sortedPoints[0][1]
+
+        # greedily find longest sequence of overlapping intervals at a time
+        for point in sortedPoints:
+            # current (and future) interval does not overlap with previous
+            if overlapEnd < point[0]:
+                arrowCount += 1
+                overlapEnd = point[1]
+            # current interval overlaps, update tightest intersection bound
+            else:
+                overlapEnd = min(point[1], overlapEnd)
+
+        arrowCount += 1
+        return arrowCount
