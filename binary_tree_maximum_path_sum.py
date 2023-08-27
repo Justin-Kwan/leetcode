5d5bf6ff9aa438fc063e47e2b44eb0bfc99941b4
Justin Kwan <justinpkwan@outlook.com>
2023-08-26 20:51:50 -0400

    did another leetcode

--- binary_tree_maximum_path_sum.py
+++ binary_tree_maximum_path_sum.py
@@ -5,23 +5,51 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    maxPathSumSoFar: int = float('-inf')
-
+    # top down dp approach
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.findMaxPathSum(root)
-        return self.maxPathSumSoFar
+        maxPathSum, _ = self.maxPathSumUnder(root)
+        return maxPathSum
 
-    def findMaxPathSum(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
+    def maxPathSumUnder(self, root: Optional[TreeNode]) -> Tuple[int, int]:
+        # prevent negative nodes from reporting sums of 0 since null nodes
+        # have no paths or branches
+        if not root:
+            return (float('-inf'), float('-inf'))
 
-        maxSumUpToLeft = self.findMaxPathSum(root.left)
-        maxSumUpToRight = self.findMaxPathSum(root.right)
-        maxSumUpToCurrent = max(root.val, root.val + maxSumUpToLeft + maxSumUpToRight, root.val + maxSumUpToLeft, root.val + maxSumUpToRight)
+        # compute global max path sum under current root by comparing max sum
+        # seen within left and right subtrees, and max sum ending at left and
+        # right children, and root
+        maxLeftPathSum, maxLeftBranchSum = self.maxPathSumUnder(root.left)
+        maxRightPathSum, maxRightBranchSum = self.maxPathSumUnder(root.right)
 
-        # update global max path sum if local path sum is larger
-        self.maxPathSumSoFar = max(self.maxPathSumSoFar, maxSumUpToCurrent)
+        maxBranchSum = max(root.val, root.val + maxLeftBranchSum, root.val + maxRightBranchSum)
+        maxPathSum = max(maxBranchSum, maxLeftPathSum, maxRightPathSum, root.val + maxLeftBranchSum + maxRightBranchSum)
 
-        # prevent "forcing" current node val to sum if left or right is negative
-        # current node can always skip summing with max up to left or right
-        return root.val + max(maxSumUpToLeft, maxSumUpToRight, 0)
+        # report global and local maximum sums at current node
+        return (maxPathSum, maxBranchSum)
+
+    # # dfs preorder approach
+    # def maxPathSum(self, root: Optional[TreeNode]) -> int:
+    #     maxPathSum = float('-inf')
+
+    #     # computes only local max path sum ending at node, and updating
+    #     # global variable that tracks global max path sum
+    #     def maxPathSumAt(root: Optional[TreeNode]) -> int:
+    #         nonlocal maxPathSum
+
+    #         if root is None:
+    #             return 0
+
+    #         maxSumAtLeft = maxPathSumAt(root.left)
+    #         maxSumAtRight = maxPathSumAt(root.right)
+    #         maxSumAtCurrent = max(root.val, root.val + maxSumAtLeft + maxSumAtRight, root.val + maxSumAtLeft, root.val + maxSumAtRight)
+
+    #         # update global max path sum if local path sum is larger
+    #         maxPathSum = max(maxPathSum, maxSumAtCurrent)
+
+    #         # current node value may be larger local max sum itself versus
+    #         # adding a negative max sum from left or right path
+    #         return root.val + max(maxSumAtLeft, maxSumAtRight, 0)
+
+    #     maxPathSumAt(root)
+    #     return maxPathSum
