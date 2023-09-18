262f7bd87c69c0aabe48cd6761abba2042b4271f
Justin Kwan <justinpkwan@outlook.com>
2023-09-17 20:37:13 -0400

    did another leetcode approach

--- cpp/largest_rectangle_in_histogram.cpp
+++ cpp/largest_rectangle_in_histogram.cpp
@@ -45,4 +45,38 @@ public:
         // in left or right, or max rectangle area crossing middle partition
         return max({ curMaxArea, leftMaxArea, rightMaxArea });
     }
+
+    // // optimal monotonic stack approach
+    // int largestRectangleArea(vector<int> &heights) {
+    //     // stack stores monotonically increasing heights, defer computing area of
+    //     // rectangle from it once a shorter height no longer accomondates
+    //     stack<pair<int, int>> seenHeights;
+    //     int maxArea = 0;
+
+    //     for (int i = 0; i < heights.size(); ++i) {
+    //         // assume rectangle at current bar height starts from current position
+    //         int startPos = i;
+
+    //         // reached smaller bar height, compute areas for previous taller rectangles
+    //         // since we know they end here
+    //         while (!seenHeights.empty() && seenHeights.top().first > heights[i]) {
+    //             auto &[prevHeight, prevPos] = seenHeights.top(); seenHeights.pop();
+    //             maxArea = max(prevHeight * (i - prevPos), maxArea);
+    //             // rectangle at shorter current height could start from previous
+    //             // taller height's position so carry it over
+    //             startPos = prevPos;
+    //         }
+
+    //         seenHeights.push(make_pair(heights[i], startPos));
+    //     }
+
+    //     // pop out and compute rectangle areas from any remaining increasing heights
+    //     while (!seenHeights.empty()) {
+    //         auto &[prevHeight, prevPos] = seenHeights.top(); seenHeights.pop();
+    //         int areaFromHeight = prevHeight * (heights.size() - prevPos);
+    //         maxArea = max(areaFromHeight, maxArea);
+    //     }
+
+    //     return maxArea;
+    // }
 };
